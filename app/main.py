e6cdc23ae16b43fb59e6a0253d68c8c2861e1ccc
Orzu Narziyev <@narziyevorzu>
2024-06-27 04:14:06 +0500

    change"

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 import contextlib
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import Session
+from sqlalchemy import select
 
-from .database import create_all_tables
+from .database import create_all_tables, engine
+from .models import Post
 
 
 @contextlib.asynccontextmanager
@@ -14,8 +18,10 @@ app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def get():
+    async with AsyncSession(engine) as session:
+        result = await session.execute(select(Post))
+        return result.scalars().all()
 
 
 @app.get("/hello/{name}")
--- app/models.py
+++ app/models.py
@@ -8,11 +8,13 @@ from sqlalchemy import Text, String, Integer, DateTime
 class Post(Base):
     __tablename__ = 'posts'
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    created_at: Mapped[datetime] = mapped_column(DateTime, default_factory=datetime.utcnow)
-    updated_at: Mapped[datetime] = mapped_column(DateTime, default_factory=datetime.utcnow)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
 
     title: Mapped[str] = mapped_column(String(256), nullable=False)
     content: Mapped[str] = mapped_column(Text, nullable=False)
+
+
 
 
 
