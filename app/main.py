91b07c404ad3257ca52b56b40ac886a2f7eded5f
Orzu Narziyev <@narziyevorzu>
2024-06-27 03:02:53 +0500

    change: database

--- app/main.py
+++ app/main.py
@@ -2,15 +2,14 @@ import contextlib
 
 from fastapi import FastAPI
 
-from .database import create_all_tables
+from app.database import create_all_tables, drop_all_tables
 
 
 @contextlib.asynccontextmanager
 async def lifespan(app: FastAPI):
+    await drop_all_tables()
     await create_all_tables()
     yield
-
-
 
 
 app = FastAPI(lifespan=lifespan)
